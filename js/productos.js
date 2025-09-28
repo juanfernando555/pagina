b728e2a44786fc4be0b40dcc0adcf53c99db0fbd
juanfernando555 <juanfernandomaytaflores47@gmail.com>
2025-09-27 20:25:17 -0400

    ds

---
+++ js/productos.js
@@ -0,0 +1,93 @@
+const productos = [
+  {
+    id: "producto1",
+    nombre: "ITALIANO",
+    descripcion: "Cuello italiano es un tipo de cuello de camisa caracterizado por tener las puntas ampliamente separadas entre sí.",
+    imagen: "../img-catalogo/ITALIANO.jpeg",
+    precio: 1299.00
+  },
+  {
+    id: "producto2",
+    nombre: "SEMI-ITALIANO",
+    descripcion: "El cuello semi italiano es una versión más discreta del cuello italiano.",
+    imagen: "../img-catalogo/SEMI-ITALIANO.jpeg",
+    precio: 299.00
+  },
+  {
+    id: "producto3",
+    nombre: "Producto 3",
+    descripcion: "aaa",
+    imagen: "../img-catalogo/488658179_651609164505058_436925742149740408_n.jpg",
+    precio: 149.00
+  },
+  {
+    id: "producto4",
+    nombre: "Producto 4",
+    descripcion: "Descripción breve del producto 4.",
+    imagen: "../img-catalogo/480761882_617057601293548_95604407827483747_n.jpg",
+    precio: 199.00
+  },
+  {
+    id: "producto5",
+    nombre: "Producto 5",
+    descripcion: "Descripción breve del producto 5.",
+    imagen: "../img-catalogo/479720357_610421115290530_7733450406145481184_n.jpg",
+    precio: 249.00
+  },
+  {
+    id: "producto6",
+    nombre: "Producto 6",
+    descripcion: "Descripción breve del producto 6.",
+    imagen: "../img-catalogo/477064577_610420998623875_7372694078387625839_n.jpg",
+    precio: 349.00
+  },
+  {
+    id: "producto7",
+    nombre: "Producto 7",
+    descripcion: "Descripción breve del producto 7.",
+    imagen: "../img-catalogo/477978736_609624772036831_6638760143252815705_n.jpg",
+    precio: 179.00
+  },
+  {
+    id: "producto8",
+    nombre: "Producto 8",
+    descripcion: "Descripción breve del producto 8.",
+    imagen: "../img-catalogo/476125808_601862236146418_1539294755732078154_n.jpg",
+    precio: 399.00
+  },
+  {
+    id: "producto9",
+    nombre: "Producto 9",
+    descripcion: "Descripción breve del producto 9.",
+    imagen: "../img-catalogo/475794553_601862032813105_528445780155454400_n.jpg",
+    precio: 299.00
+  },
+  {
+    id: "producto10",
+    nombre: "Producto 10",
+    descripcion: "Descripción breve del producto 10.",
+    imagen: "../img-catalogo/475788368_601453646187277_3391182315820079177_n.jpg",
+    precio: 279.00
+  },
+  {
+    id: "producto11",
+    nombre: "Producto 11",
+    descripcion: "Descripción breve del producto 11.",
+    imagen: "../img-catalogo/475288876_599003359765639_7404527504092965500_n.jpg",
+    precio: 189.00
+  },
+  {
+    id: "producto12",
+    nombre: "Producto 12",
+    descripcion: "Descripción breve del producto 12.",
+    imagen: "../img-catalogo/473106491_586933667639275_7450342377372598872_n.jpg",
+    precio: 159.00
+  },
+  {
+    id: "producto13",
+    nombre: "Producto 13",
+    descripcion: "Descripción breve del producto 13.",
+    imagen: "../img-catalogo/473043012_586102511055724_8362056363693836796_n.jpg",
+    precio: 209.00
+  }
+]; 
